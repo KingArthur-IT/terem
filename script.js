060bc15ecd3430bee8caeb963b89925ea054c92d
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-07-02 23:14:24 +0300

    designers desctop

---
+++ script.js
@@ -0,0 +1,21 @@
+$(document).ready(function(){
+    $('.designers-projects-slider__wrapper').slick({
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.designers-projects-slider__left-arrow'),
+        nextArrow: $('.designers-projects-slider__right-arrow')
+    });
+});
+
+$(document).ready(function(){
+    $('.designers-slider__wrapper').slick({
+        autoplay: true,
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        prevArrow: $('.designers-slider__left-arrow'),
+        nextArrow: $('.designers-slider__right-arrow')
+    });
+});
