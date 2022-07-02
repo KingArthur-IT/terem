b2908985d462757d9b0a4a2904afaaf21f2d57d8
Artem <67517973+KingArthur-IT@users.noreply.github.com>
2022-07-03 00:43:19 +0300

    designers page adaptive

--- script.js
+++ script.js
@@ -5,17 +5,45 @@ $(document).ready(function(){
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: $('.designers-projects-slider__left-arrow'),
-        nextArrow: $('.designers-projects-slider__right-arrow')
+        nextArrow: $('.designers-projects-slider__right-arrow'),
+        responsive: [
+            {
+              breakpoint: 800,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 400,
+                settings: {
+                  slidesToShow: 1,
+                }
+              },
+        ]
     });
 });
 
 $(document).ready(function(){
     $('.designers-slider__wrapper').slick({
-        autoplay: true,
+        autoplay: false,
         infinite: true,
         slidesToShow: 3,
         slidesToScroll: 1,
         prevArrow: $('.designers-slider__left-arrow'),
-        nextArrow: $('.designers-slider__right-arrow')
+        nextArrow: $('.designers-slider__right-arrow'),
+        responsive: [
+            {
+              breakpoint: 1000,
+              settings: {
+                slidesToShow: 2,
+              }
+            },
+            {
+                breakpoint: 800,
+                settings: {
+                  slidesToShow: 1,
+                }
+              },
+        ]
     });
 });
